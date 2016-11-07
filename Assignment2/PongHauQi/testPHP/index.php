38bf01f2145187be9f4076c279bf7461dc19fe3b
ShingJan <yuanj.shi@hotmail.com>
2016-11-07 11:29:52 +0800

    a little update

--- Assignment2/PongHauQi/testPHP/index.php
+++ Assignment2/PongHauQi/testPHP/index.php
@@ -158,47 +158,47 @@ a:hover {
     <script type="text/javascript"
         src="https://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
     <script type="text/javascript">
-// jQuery Document
-$(document).ready(function(){
-});
+        // jQuery Document
+        $(document).ready(function(){
+        });
  
-//jQuery Document
-$(document).ready(function(){
-    //If user wants to end session
-    $("#exit").click(function(){
-        var exit = confirm("Are you sure you want to end the session?");
-        if(exit==true){window.location = 'index.php?logout=true';}     
-    });
-});
+        //jQuery Document
+        $(document).ready(function(){
+            //If user wants to end session
+            $("#exit").click(function(){
+                var exit = confirm("Are you sure you want to end the session?");
+                if(exit==true){window.location = 'index.php?logout=true';}
+            });
+        });
  
-//If user submits the form
-$("#submitmsg").click(function(){
-        var clientmsg = $("#usermsg").val();
-        $.post("post.php", {text: clientmsg});             
-        $("#usermsg").attr("value", "");
-        loadLog;
-    return false;
-});
+        //If user submits the form
+        $("#submitmsg").click(function(){
+                var clientmsg = $("#usermsg").val();
+                $.post("post.php", {text: clientmsg});
+                $("#usermsg").attr("value", "");
+                loadLog;
+                return false;
+            });
  
-function loadLog(){    
-    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
-    $.ajax({
-        url: "log.html",
-        cache: false,
-        success: function(html){       
-            $("#chatbox").html(html); //Insert chat log into the #chatbox div  
+        function loadLog(){
+            var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+            $.ajax({
+                url: "log.html",
+                cache: false,
+                success: function(html){
+                    $("#chatbox").html(html); //Insert chat log into the #chatbox div
            
-            //Auto-scroll          
-            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
-            if(newscrollHeight > oldscrollHeight){
-                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-            }              
-        },
-    });
-}
+                //Auto-scroll
+                var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+                    if(newscrollHeight > oldscrollHeight){
+                        $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                    }
+                },
+            });
+        }
  
-setInterval (loadLog, 2500);
-</script>
+        setInterval (loadLog, 1000);
+    </script>
 <?php
     }
     ?>
