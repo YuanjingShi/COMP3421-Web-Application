8a1b2b8ee8f66616d9b9c14281cca2fa61896819
ShingJan <yuanj.shi@hotmail.com>
2016-11-15 23:28:12 +0800

    get rid of all the hash stuff and work on Geometry now

--- Assignment2/PongHauQi/testPHP/checkLogin.php
+++ Assignment2/PongHauQi/testPHP/checkLogin.php
@@ -1,9 +1,6 @@
 <?php
 session_start ();
-  $options = [
-    'cost' => 11,
-    //'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
-    ];
+  
     $temp = password_hash($_SESSION ['pwd'], PASSWORD_BCRYPT, $options);
     //echo $temp;
     //echo $_SESSION['array'][$_SESSION['name']];
--- Assignment2/PongHauQi/testPHP/login.php
+++ Assignment2/PongHauQi/testPHP/login.php
@@ -1,32 +1,50 @@
 <?php
 session_start ();
-$count = 0;
-$array = array(
-    "issac" => array(1),
-    "henry" => array(2),
-);
-    if (isset ( $_POST ['enter'] )) {
-        if ($_POST ['name'] != "") {
-            foreach ($array as $i => $value) {
-                if($array[$i] == stripslashes ( htmlspecialchars ( $_POST ['name'] ) )){
-                    $count = $count + 1;
-             }
-            }
-            if($count == 0){
-                $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
-                $array[] = $_SESSION ['name'];
-                $fp = fopen ( "user.txt", 'a' );
-                fwrite ( $fp, $_SESSION ['name'] . " has won". );
-                fclose ( $fp );
-            else{
 
-            }
+$file = "user.txt";
+$fopen = fopen ( $file, "r");
+
+if ($fopen) {
+    $text = explode("\n", fread($fopen, filesize($file)));
+}
+fclose($fopen);
+$count = count($text);
+$_SESSION['array'] = array();
+$_SESSION['user'] = array();
+for($i=0;$i<$count;$i++){
+    list($user,$pwd) = explode(" ", $text[$i]);
+    $_SESSION['user'][] = $user;
+    $_SESSION['array'][$user] = $pwd;
+}
+//echo in_array(henry,$text,true) ? 'It is here' : 'Sorry it is not';
+//print_r($_SESSION['user']);
+//print_r($_SESSION['array']["henry"]);
 
 
-        } else {
-            echo '<span class="error">Please type in a name</span>';
+
+if (isset ( $_POST ['enter'] )) {
+    if ($_POST ['name'] != "" && $_POST ['pwd'] != "") {
+        $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
+        $_SESSION ['pwd'] = stripslashes ( htmlspecialchars ( $_POST ['pwd'] ) );
+        //echo $_POST ['name'];
+        if(in_array($_SESSION ['name'], $_SESSION['user'])){
+            //echo $_SESSION['array'][$_SESSION['name']];
+            header("Location: checkLogin.php");
+        }else{
+            //echo '<span class="error">User is not registered!</span>';
+            $fp1 = fopen ( $file, 'a' );
+            fwrite ($fp1, $_SESSION ['name']." ".$_SESSION ["pwd"]."\n");
+            fclose ( $fp1 );
+            $fp = fopen ( "log.html", 'a' );
+            fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat session.</i><br></div>" );
+            fclose ( $fp );
+            header("Location: index.php");
         }
+
+    } else {
+        echo '<span class="error">Please input sth valid</span>';
     }
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
