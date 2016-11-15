a241aafcede9d6c792649778b6c38e2fe3f83f9d
ShingJan <yuanj.shi@hotmail.com>
2016-11-15 23:21:23 +0800

    change on login and try to figure out some location tricks with Google map api

--- Assignment2/PongHauQi/testPHP/login.php
+++ Assignment2/PongHauQi/testPHP/login.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<link rel="stylesheet" type="text/css" href="mystyle.css" media="screen" />
-<body>
 <?php
 session_start ();
 $count = 0;
@@ -10,21 +5,6 @@ $array = array(
     "issac" => array(1),
     "henry" => array(2),
 );
-function loginForm() {
-    echo '
-   <div id="loginform">
-   <form action="index.php" method="post">
-       <p>Please enter your name to continue:</p>
-       <label for="name">Name:</label>
-       <input type="text" name="name" id="name" />
-       <label for="country">Geo Info:</label>
-       <input type="text" name="country" id="country" />
-       <input type="submit" name="enter" id="enter" value="Enter" />
-   </form>
-   </div>
-   ';
-}
-    loginForm();
     if (isset ( $_POST ['enter'] )) {
         if ($_POST ['name'] != "") {
             foreach ($array as $i => $value) {
@@ -48,6 +28,21 @@ function loginForm() {
         }
     }
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" type="text/css" href="mystyle.css" media="screen" />
+<body>
+ <div id="loginform">
+   <form action="index.php" method="post">
+       <p>Please enter your name to continue:</p>
+       <label for="name">Name:</label>
+       <input type="text" name="name" id="name" />
+       <label for="country">Geo Info:</label>
+       <input type="text" name="country" id="country" />
+       <input type="submit" name="enter" id="enter" value="Enter" />
+   </form>
+   </div>
 </body>
 </head>
 </html>
